40327926d5f7d6c93adaf4cacb162483fea99c7c
vidhyasaharan <v.sethu@unsw.edu.au>
2022-01-01 18:55:14 +1100

    Corrected mahalanobis to square mahalanobis

--- src/VectorDataUtils.jl
+++ src/VectorDataUtils.jl
@@ -1,13 +1,15 @@
 module VectorDataUtils
 
 using LoopVectorization
+using LinearAlgebra
 
 export l2inner, l2norm, sql2norm, l2dist, sql2dist
 export l1norm, l1dist
-export xAx, mahaldist
+export xAx, mahaldist, sqmahaldist
 export running_mean, running_mean!, update_running_mean!
 export running_meanvar, running_meanvar!, update_running_meanvar!
 export running_meancov, running_meancov!, update_running_meancov!
+
 
 include("L2.jl")
 include("Lp.jl")
