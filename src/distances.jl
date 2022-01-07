42d4c58a2b5259cc96e2bd0a36c4f17322d755d2
vidhyasaharan <v.sethu@unsw.edu.au>
2022-01-07 23:46:52 +1100

    extending functions from Distances.jl internally

--- src/distances.jl
+++ src/distances.jl
@@ -1,17 +1,43 @@
+import Distances.pairwise!
+import Distances.pairwise
+import Distances.colwise!
+import Distances.colwise
+import Distances.evaluate
+
 ## Generic distance evaluation
 #define Abstract type Distance
 abstract type Distance end
 struct sql2 <: Distance end
 struct l2 <: Distance end
 struct l1 <: Distance end
-dist(::sql2, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = sql2dist(a,b)
-dist(::l2, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = l2dist(a,b)
-dist(::l1, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = l1dist(a,b)
+evaluate(::sql2, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = sql2dist(a,b)
+evaluate(::l2, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = l2dist(a,b)
+evaluate(::l1, a::AbstractVector{T}, b::AbstractVector{T}) where {T} = l1dist(a,b)
+
+#Columnwise distances
+function colwise!(d::AbstractVector{<:AbstractFloat}, dm::Distance, x::AbstractMatrix{T}, y::AbstractMatrix{T}) where {T}
+    @inbounds @views Threads.@threads for i ∈ axes(y,2)
+        d[i] = evaluate(dm, x[:,i], y[:,i])
+    end
+end
+
+function colwise!(d::AbstractVector{<:AbstractFloat}, dm::Distance, x::AbstractVector{T}, y::AbstractMatrix{T}) where {T}
+    @inbounds @views Threads.@threads for i ∈ axes(y,2)
+        d[i] = evaluate(dm,x,y[:,i])
+    end
+end
+
+function colwise(dm::Distance, x::AbstractVecOrMat{T}, y::AbstractMatrix{T}) where {T}
+    d = Vector{T}(undef,size(y,2))
+    colwise!(d,dm,x,y)
+    return d
+end
+
 
 #Pairwise distances
 function pairwise!(d::AbstractVector{<:AbstractFloat}, dm::Distance, x::AbstractVector{T}, y::AbstractMatrix{T}) where {T}
     @inbounds @views Threads.@threads for i ∈ axes(y,2)
-        d[i] = dist(dm, x, y[:,i])
+        d[i] = evaluate(dm, x, y[:,i])
     end
 end
 
@@ -20,7 +46,7 @@ pairwise!(d::AbstractVector{<:AbstractFloat}, dm::Distance, x::AbstractMatrix{T}
 function pairwise!(d::AbstractMatrix{<:AbstractFloat}, dm::Distance, x::AbstractMatrix{T}, y::AbstractMatrix{T}) where {T}
     @inbounds @views Threads.@threads for i ∈ axes(x,2)
         for j ∈ axes(y,2)
-            d[i,j] = dist(dm, x[:,i], y[:,j])
+            d[i,j] = evaluate(dm, x[:,i], y[:,j])
         end
     end
 end
--- test/distances_tests.jl
+++ test/distances_tests.jl
@@ -3,33 +3,49 @@
     b = randn(Float,10)
     c1 = [a b a b]
     c2 = [a b]
+    c3 = [a b b b]
 
     @test VectorDataUtils.sql2 <: VectorDataUtils.Distance
     @test VectorDataUtils.l2 <: VectorDataUtils.Distance
     @test VectorDataUtils.l1 <: VectorDataUtils.Distance
 
-    @test VectorDataUtils.dist(VectorDataUtils.sql2(),a,b) ≈ VectorDataUtils.sql2dist(a,b)
-    @test VectorDataUtils.dist(VectorDataUtils.l2(),a,b) ≈ VectorDataUtils.l2dist(a,b)
-    @test VectorDataUtils.dist(VectorDataUtils.l1(),a,b) ≈ VectorDataUtils.l1dist(a,b)
+    @test VectorDataUtils.evaluate(VectorDataUtils.sql2(),a,b) ≈ VectorDataUtils.sql2dist(a,b)
+    @test VectorDataUtils.evaluate(VectorDataUtils.l2(),a,b) ≈ VectorDataUtils.l2dist(a,b)
+    @test VectorDataUtils.evaluate(VectorDataUtils.l1(),a,b) ≈ VectorDataUtils.l1dist(a,b)
 
     distances = (VectorDataUtils.sql2(), VectorDataUtils.l2(), VectorDataUtils.l1())
     for dm ∈ distances
+
+        d = VectorDataUtils.colwise(dm,c1,c3)
+        @test length(d) == size(c1,2)
+        @test d[1] == 0.0
+        @test d[2] == 0.0
+        @test d[3] == VectorDataUtils.evaluate(dm,a,b)
+
+        d = VectorDataUtils.colwise(dm,a,c1)
+        @test length(d) == size(c1,2)
+        @test d[1] == 0.0
+        @test d[2] == VectorDataUtils.evaluate(dm,a,b)
+        @test d[3] == 0.0
+        @test d[4] == d[2]
+
+
         d = VectorDataUtils.pairwise(dm,a,c1)
         @test length(d) == size(c1,2)
         @test d[1] == 0.0
-        @test d[2] == VectorDataUtils.dist(dm, a, b)
+        @test d[2] == VectorDataUtils.evaluate(dm, a, b)
 
         d = VectorDataUtils.pairwise(dm,c1,a)
         @test length(d) == size(c1,2)
         @test d[3] == 0.0
-        @test d[4] == VectorDataUtils.dist(dm, a, b)
+        @test d[4] == VectorDataUtils.evaluate(dm, a, b)
 
         dd = VectorDataUtils.pairwise(dm, c2, c1)
         @test size(dd,1) == size(c2,2)
         @test size(dd,2) == size(c1,2)
         @test dd[1,1] == 0.0
         @test dd[2,2] == 0.0
-        @test dd[1,2] == VectorDataUtils.dist(dm, a, b)
-        @test dd[2,1] == VectorDataUtils.dist(dm, b, a)
+        @test dd[1,2] == VectorDataUtils.evaluate(dm, a, b)
+        @test dd[2,1] == VectorDataUtils.evaluate(dm, b, a)
     end
 end
